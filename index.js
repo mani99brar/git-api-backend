e50b128f7e7345eab321f4e48a7acc2bb9ff392a
mani99brar <mani99brar@gmail.com>
2024-01-03 16:11:54 +0530

    fix:flow

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const axios = require('axios');
 const session = require('express-session');
 const cors = require('cors');
+const path = require('path');
 
 
 const app = express();
-
+app.use(express.static('public'));
 app.use(cors());
 // Configure session middleware
 app.use(session({
@@ -23,43 +24,43 @@ const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 app.get('/oauth-callback', async (req, res) => {
     const requestToken = req.query.code; // Get the code from the query parameter
+    res.send(CLIENT_ID + " " + CLIENT_SECRET + " " + requestToken);
+    // axios({
+    //     method: 'post',
+    //     url: `https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${requestToken}`,
+    //     headers: {
+    //         accept: 'application/json'
+    //     }
+    // }).then(async (response) => {
+    //     // Store the access token in session data
+    //     // req.session.accessToken = response.data.access_token;
+    //     res.send(response.data);
+    //     // Fetch user information to get the username
+    //     // try {
+    //     //     const userResponse = await axios.get('https://api.github.com/user', {
+    //     //         headers: {
+    //     //             'Authorization': `Bearer ${req.session.accessToken}`
+    //     //         }
+    //     //     });
+    //     //     req.session.owner = userResponse.data.login; // Store the owner's username in the session
 
-    axios({
-        method: 'post',
-        url: `https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${requestToken}`,
-        headers: {
-            accept: 'application/json'
-        }
-    }).then(async (response) => {
-        // Store the access token in session data
-        // req.session.accessToken = response.data.access_token;
-        res.send(response.data);
-        // Fetch user information to get the username
-        // try {
-        //     const userResponse = await axios.get('https://api.github.com/user', {
-        //         headers: {
-        //             'Authorization': `Bearer ${req.session.accessToken}`
-        //         }
-        //     });
-        //     req.session.owner = userResponse.data.login; // Store the owner's username in the session
+    //     //     // Continue to fetch user's repositories
+    //     //     const reposResponse = await axios.get('https://api.github.com/user/repos', {
+    //     //         headers: {
+    //     //             'Authorization': `Bearer ${req.session.accessToken}`
+    //     //         }
+    //     //     });
 
-        //     // Continue to fetch user's repositories
-        //     const reposResponse = await axios.get('https://api.github.com/user/repos', {
-        //         headers: {
-        //             'Authorization': `Bearer ${req.session.accessToken}`
-        //         }
-        //     });
+    //     //     // Send back repositories data as response or handle accordingly
+    //     //     res.send(reposResponse.data);
 
-        //     // Send back repositories data as response or handle accordingly
-        //     res.send(reposResponse.data);
-
-        // } catch (error) {
-        //     console.error("Error fetching user or repositories", error);
-        //     res.send("Error during fetching user or repositories",error);
-        // }
-    }).catch(error => {
-        res.send("Error during token exchange WWA: " + error);
-    });
+    //     // } catch (error) {
+    //     //     console.error("Error fetching user or repositories", error);
+    //     //     res.send("Error during fetching user or repositories",error);
+    //     // }
+    // }).catch(error => {
+    //     res.send("Error during token exchange WWA: " + error);
+    // });
 });
 
 app.get('/session-data', (req, res) => {
@@ -143,6 +144,9 @@ app.get('/save-file', async (req, res) => {
 app.get('/welcome', (req, res) => {
     // Handle the welcome page after the user is authenticated
     res.send("You're successfully authenticated. Welcome!");
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html')); // Adjust with the actual path to your HTML file
 });
 
 const PORT = process.env.PORT || 3001;
