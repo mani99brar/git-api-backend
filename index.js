dc66921dd22b7043836fb27ba6be6798fe20b158
mani99brar <mani99brar@gmail.com>
2024-01-03 15:43:15 +0530

    fix:error handling

--- index.js
+++ index.js
@@ -32,33 +32,33 @@ app.get('/oauth-callback', async (req, res) => {
         }
     }).then(async (response) => {
         // Store the access token in session data
-        req.session.accessToken = response.data.access_token;
-        
-        // Fetch user information to get the username
-        try {
-            const userResponse = await axios.get('https://api.github.com/user', {
-                headers: {
-                    'Authorization': `Bearer ${req.session.accessToken}`
-                }
-            });
-            req.session.owner = userResponse.data.login; // Store the owner's username in the session
+        // req.session.accessToken = response.data.access_token;
+        res.send(response.data);
+        // // Fetch user information to get the username
+        // try {
+        //     const userResponse = await axios.get('https://api.github.com/user', {
+        //         headers: {
+        //             'Authorization': `Bearer ${req.session.accessToken}`
+        //         }
+        //     });
+        //     req.session.owner = userResponse.data.login; // Store the owner's username in the session
 
-            // Continue to fetch user's repositories
-            const reposResponse = await axios.get('https://api.github.com/user/repos', {
-                headers: {
-                    'Authorization': `Bearer ${req.session.accessToken}`
-                }
-            });
+        //     // Continue to fetch user's repositories
+        //     const reposResponse = await axios.get('https://api.github.com/user/repos', {
+        //         headers: {
+        //             'Authorization': `Bearer ${req.session.accessToken}`
+        //         }
+        //     });
 
-            // Send back repositories data as response or handle accordingly
-            res.send(reposResponse.data);
+        //     // Send back repositories data as response or handle accordingly
+        //     res.send(reposResponse.data);
 
-        } catch (error) {
-            console.error("Error fetching user or repositories", error);
-            res.status(500).send("Error during fetching user or repositories",error);
-        }
+        // } catch (error) {
+        //     console.error("Error fetching user or repositories", error);
+        //     res.send("Error during fetching user or repositories",error);
+        // }
     }).catch(error => {
-        res.status(500).send("Error during token exchange: " + error.message);
+        res.send("Error during token exchange WW: " + error);
     });
 });
 
